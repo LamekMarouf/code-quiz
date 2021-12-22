a3a7a569f4309384e74367bd03af2985207d9d7e
Lamek Marouf <lamekmaroufjob@hotmail.com>
2021-12-23 10:37:01 +1100

    added js questions and answers

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is 2+2?",
+    answer: "4",
+    options: [
+      "2",
+      "4",
+      "6",
+      "8"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many letters are there in the alphabet?",
+    answer: "26",
+    options: [
+      "21",
+      "25",
+      "26",
+      "U R A Q T"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What colour is the sky at night?",
+    answer: "Black",
+    options: [
+      "Blue",
+      "Stars",
+      "White",
+      "Black"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of these birds cannot fly?",
+    answer: "Penguin",
+    options: [
+      "Dove",
+      "Pigeon",
+      "Alligator",
+      "Penguin"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is 4+8*2-2/2+12",
+    answer: "31",
+    options: [
+      "11",
+      "31",
+      "23",
+      "21"
+    ]
+  },
+];
